431f51cf89ad2ef54e64a62ac5665f0d4e4bef5c
rohit mishra <rohit.mishra@raweng.com>
2018-09-19 11:44:34 +0530

    make it better

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
 
- /* if($_SERVER['PHP_AUTH_USER'] !== 'demo' && $_SERVER['PHP_AUTH_PW'] !== 'demo1234') 
+  if($_SERVER['PHP_AUTH_USER'] !== 'demo' && $_SERVER['PHP_AUTH_PW'] !== 'demo1234') 
   {
   		header("WWW-Authenticate: Basic realm=\"thetutlage\"");
   		header("HTTTP\ 1.0 401 Unauthorized");
   		echo 'There is error';
   		exit;
-  }*/
+  }
 
 header( 'Location: ./main.html' );
 
