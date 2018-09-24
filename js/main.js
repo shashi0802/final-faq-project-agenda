1ddb17edd0f0ed9060d18de835723434658ade27
rohit mishra <rohit.mishra@raweng.com>
2018-09-24 15:11:35 +0530

    updated the design

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-// $( window ).load(function() {
-// console.log("sdfbhgjfcghjdsvcfhdj")
-    
-
-//     $(".author-name").click(function () {
-//     console.log("i m inside")
-//     $('.detail').empty();
-//     var $popdata = $(this).parents('.row').next('.detail-content').clone()
-//     var $htmllll = $('.detail').html($popdata[0]);
-//     $(".detail").fadeIn();
-//     $("body").css({"overflow":"hidden"});
-// });
-// });
-
-// $(document).ready(function() {
-//     $("h3").click(function(){
-//         alert("The paragraph was clicked.");
-//     });
-// });
-
-
 $(window).on('load', function() {
     $(document).on("click", ".author-name", function() {
        $('.detail').empty();
@@ -42,5 +21,44 @@ $(window).on('load', function() {
             $(".detail").fadeOut();
             $("body").css({"overflow":"scroll"});
         }
+    });
+    // jquery code for signin and forgot page
+    $("input").click(function () {
+        $(this).prev().css({ "display": "block", "margin-bottom": "0rem" });
+    });
+
+    $("input").blur(function () {
+        if ($(this).val().length <= 0) {
+            $(this).prev().css({ "display": "none" });   
+        }
+    });
+
+    $(".vertical-dots").click(function (e) {
+        console.log("click");
+        e.stopPropagation();
+        $(this).next(".notification").css({ "display": "block", "margin-bottom": "0rem" });
+    });
+
+    $(document).on('click', function (e) {
+        $(".notification").hide();
+    });
+
+    // keypress event
+    $('input[type="text"]').keypress(function () {
+        $("#email-err").show();
+    });
+    $('input[type="password"]').keyup(function () {
+        console.log($(this).val().length);
+        if (($(this).val().length) == 0) {
+            $("#password-err").show();
+        } else {
+            $("#password-err").hide();
+        }
+    });
+
+    // forgot page
+    $(".night-mood").click(function(){
+        $(this).parents(".notification").css({"background-color":"#333333"});
+        $("body").css({"background-color":"#333333"});
     });
 });
